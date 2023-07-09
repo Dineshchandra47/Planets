3a5348a5cb6ece6c2ca9ca1c52efd66ae50a9482
Dineshchandra47 <dineshchandraindia5@gmail.com>
2023-07-09 21:11:42 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,50 @@ import './App.css'
 
 const App = ()=> {
   return <>
-        cosnt location = useLocation()
-        const [active,setActivePlanet] = useState('/') 
+        const location = useLocation();
+    const [activePlanet, setActivePlanet] = useState('/');
 
-        return (
-          <Provider>
+    return (
+        <Provider>
             <Wrapper>
-            <Navbar
+                <Navbar
                     pathName={location.pathname}
                     onHover={setActivePlanet}
                     activePlanet={activePlanet}
-                />
+               activePlanet={activePlanet}
+               />
+
+              <AnimatePresence>
+                    <Switch location={location} key={location.key}>
+                        <Route exact path="/mercury">
+                            <Mercury />
+                        </Route>
+                        <Route exact path="/venus">
+                            <Venus />
+                        </Route>
+                        <Route exact path="/earth">
+                            <Earth />
+                        </Route>
+                        <Route exact path="/mars">
+                            <Mars />
+                        </Route>
+                        <Route exact path="/jupiter">
+                            <Jupiter />
+                        </Route>
+                        <Route exact path="/saturn">
+                            <Saturn />
+                        </Route>
+                        <Route exact path="/uranus">
+                            <Uranus />
+                        </Route>
+                        <Route exact path="/neptune">
+                            <Neptune />
+                        </Route>
+                        <Route exact path="/">
+                            <KeyVisual activePlanet={activePlanet} />
+                        </Route>
+                    </Switch>
+                </AnimatePresence>
             </Wrapper>
           </Provider>
         )                 

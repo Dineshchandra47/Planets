bdfc11c2bdc9d3beb9eb3cc3666287e54740dabb
Dineshchandra47 <dineshchandraindia5@gmail.com>
2023-07-11 17:37:45 +0530

    Updated Icon.jsx

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -4,6 +4,7 @@ import iconSet from "../../selection.json";
 export const ICon = ({ name, size, color, customStyle }) => {
   return (
     <IcoMoon
+    
       iconSet={iconSet}
       disableFill="ture"
       icon={name}

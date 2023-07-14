83011b3cf6b6da40c42d82149dc7f918261319f1
Dineshchandra47 <dineshchandraindia5@gmail.com>
2023-07-14 19:47:58 +0530

    Added Illustration  Component🐛

---
+++ src/components/PlanetSection/Illustration/IllustrationStyles.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import {motion} from 'framer-motion'
+
+export const Illustration = styled(motion.div)`
+  height: 100vw;
+  max-height: 300px;
+  padding: 0 240px;
+  z-index: 4;
+
+  
+`

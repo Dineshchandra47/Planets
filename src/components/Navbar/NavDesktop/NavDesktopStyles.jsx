f87bb9dfaa9f5c5ddfa06a2d792032f321e11276
Dineshchandra47 <dineshchandraindia5@gmail.com>
2023-07-14 19:22:11 +0530

    Updated Navbar Component🐛

--- src/components/Navbar/NavDesktop/NavDesktopStyles.jsx
+++ src/components/Navbar/NavDesktop/NavDesktopStyles.jsx
@@ -1,4 +1,6 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
 
 export const Nav = styled.nav`
     height: 100%;
@@ -16,3 +18,17 @@ export const List = styled.ul`
         height: 100%;
     }
 `;
+
+export const Link = styled(NavLink)`
+    display: flex;
+    position: relative;
+    font-size: 0.6875rem;
+    font-weight: 600;
+    letter-spacing: 1px;
+    line-height: 25px;
+    text-transform: uppercase;
+    color: ${(props) => props.theme.colors.whiteAlpha75};
+    cursor: pointer;
+    transition: color 300ms ease;
+
+`

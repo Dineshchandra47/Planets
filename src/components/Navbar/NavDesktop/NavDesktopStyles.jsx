dab47d0ead6f4526766a68bf8bacb18a147be094
Dineshchandra47 <dineshchandraindia5@gmail.com>
2023-07-14 19:20:38 +0530

    Updated Navbar Component🐛

--- src/components/Navbar/NavDesktop/NavDesktopStyles.jsx
+++ src/components/Navbar/NavDesktop/NavDesktopStyles.jsx
@@ -3,3 +3,16 @@ import styled from 'styled-components';
 export const Nav = styled.nav`
     height: 100%;
 `;
+
+export const List = styled.ul`
+    display: flex;
+    flex-flow: row;
+    gap: 32px;
+    width: 100%;
+    margin-top: 39px;
+
+    @media (min-width: 1025px) {
+        margin-top: 0;
+        height: 100%;
+    }
+`;

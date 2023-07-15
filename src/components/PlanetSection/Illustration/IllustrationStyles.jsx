a37fb23cb7723ee9ace4c27d9493de55ce9e67fb
Dineshchandra47 <dineshchandraindia5@gmail.com>
2023-07-15 13:00:48 +0530

    Updated PlanetSection

--- src/components/PlanetSection/Illustration/IllustrationStyles.jsx
+++ src/components/PlanetSection/Illustration/IllustrationStyles.jsx
@@ -2,10 +2,6 @@ import styled from 'styled-components'
 import {motion} from 'framer-motion'
 
 export const Illustration = styled(motion.div)`
-  height: 100vw;
-  max-height: 300px;
-  padding: 0 240px;
-  z-index: 4;
 
   
 `
